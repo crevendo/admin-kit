cd55df40a6ee8230ea428d29d78f83523d9c1a5e
jarlisteo <jarlisteo@gmail.com>
2023-02-21 16:16:48 -0600

    add btn click event

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,3 +1,1 @@
-<button class="bg-indigo-500 py-2 px-4 rounded text-white uppercase border-none"
-  ><slot /></button
->
+<button on:click class="bg-indigo-500 py-2 px-4 rounded text-white uppercase border-none"><slot /></button>
