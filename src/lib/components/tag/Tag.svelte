8aba79a31ff80e9cae9a559d92c6a9381073c397
jarlisteo <jarlisteo@gmail.com>
2023-02-22 18:46:15 -0600

    fix

--- src/lib/components/tag/Tag.svelte
+++ src/lib/components/tag/Tag.svelte
@@ -2,16 +2,18 @@
   export let color: string;
 </script>
 
-{#if color === "yellow"}
-  <p class="inline-flex rounded-full bg-yellow-100 px-2 text-xs font-semibold leading-5 text-yellow-800 ml-3">
-    <slot />
-  </p>
-{:else if color === "red"}
-  <p class="inline-flex rounded-full bg-red-100 px-2 text-xs font-semibold leading-5 text-red-800 ml-3">
-    <slot />
-  </p>
-{:else}
-  <p class="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800 ml-3">
-    <slot />
-  </p>
-{/if}
+<button on:click>
+  {#if color === "yellow"}
+    <p class="inline-flex rounded-full bg-yellow-100 px-2 text-xs font-semibold leading-5 text-yellow-800 ml-3">
+      <slot />
+    </p>
+  {:else if color === "red"}
+    <p class="inline-flex rounded-full bg-red-100 px-2 text-xs font-semibold leading-5 text-red-800 ml-3">
+      <slot />
+    </p>
+  {:else}
+    <p class="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800 ml-3">
+      <slot />
+    </p>
+  {/if}
+</button>
